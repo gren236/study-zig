5c22ee022247d5b2f86e26e2640cc49d9700dc0c
Egor Rudkov <gren236@gmail.com>
2025-06-01 17:26:07 +0200

    finish error

--- src/errors/main.zig
+++ src/errors/main.zig
@@ -48,3 +48,42 @@ fn testCatch() []const u8 {
 test "catch" {
     try std.testing.expectEqualStrings(testCatch(), "test");
 }
+
+// errdefer
+const Foo = struct {
+    bar: u8,
+};
+
+fn testErrDefer(allocator: std.mem.Allocator) !Foo {
+    const foo = try allocator.create(Foo);
+    errdefer allocator.destroy(foo);
+
+    try testReturnsError();
+
+    return foo.*;
+}
+
+fn testReturnsError() !void {
+    return error.Baz;
+}
+
+test "errdefer" {
+    try expectError(error.Baz, testErrDefer(std.testing.allocator));
+}
+
+// unions
+const TestUnion = union(enum) {
+    hello: u8,
+    world: u8,
+};
+
+test "unions" {
+    const foo = TestUnion{
+        .hello = 7,
+    };
+
+    switch (foo) {
+        .hello => |val| try expect(val == 7),
+        .world => try expect(true),
+    }
+}
